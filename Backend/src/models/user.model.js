49837770035f0e11610c5088f2222743f40d15f5
Pranita Kawadkar <pranitakawadkar@Pranitas-MacBook-Air.local>
2026-05-14 23:18:24 +0530

    Implement user schema with validation

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    select: false,
+    minlength: [6, "Password must be at least 6 characters"],
+  },
+}, {
+  timestamps: true
+});
+
+export const  userModel = mongoose.model("User", userSchema);
