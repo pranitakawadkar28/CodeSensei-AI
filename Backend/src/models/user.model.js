ab0772c94265dca695615c01d612346be63a733e
Pranita Kawadkar <pranitakawadkar@Pranitas-MacBook-Air.local>
2026-05-15 00:27:22 +0530

    update user schema

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -3,24 +3,29 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Name is required"],
-    trim: true,
+    required: true
   },
   email: {
     type: String,
-    required: [true, "Email is required"],
+    required: true,
     unique: true,
-    lowercase: true,
-    trim: true,
   },
   password: {
     type: String,
-    required: [true, "Password is required"],
+    required: true,
     select: false,
-    minlength: [6, "Password must be at least 6 characters"],
   },
 }, {
   timestamps: true
 });
+
+userSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    delete ret.password;
+    delete ret.__v;
+    return ret;
+  },
+});
+
 
 export const  User = mongoose.model("User", userSchema);
